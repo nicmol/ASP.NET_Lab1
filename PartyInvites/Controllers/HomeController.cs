bc32023bfd3063a33b05dcda0fb3924f0a76435a
Nicolette <molitorn@my.lanecc.edu>
2019-10-05 09:47:36 -0700

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+               
+    }
+}
